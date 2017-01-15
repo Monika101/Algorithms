bc33dec4e94b760d0e9ca2220a0e777afd8d3a94
Monika101 <Monika101@users.noreply.github.com>
2017-01-15 18:30:18 +0000

    Update Greedy Algorithm.py

--- Greedy Algorithm.py
+++ Greedy Algorithm.py
@@ -8,17 +8,17 @@
 # Part A: Transporting Space Cows
 #================================
 
-def load_cows():
-    """
-    Read the contents of the given file.  Assumes the file contents contain
-    data in the form of comma-separated cow name, weight pairs, and return a
-    dictionary containing cow names as keys and corresponding weights as values.
+#def load_cows():
+    #"""
+    #Read the contents of the given file.  Assumes the file contents contain
+    #data in the form of comma-separated cow name, weight pairs, and return a
+    #dictionary containing cow names as keys and corresponding weights as values.
 
-    Parameters:
-    filename - the name of the data file as a string
+    #Parameters:
+    #filename - the name of the data file as a string
 
-    Returns:
-    a dictionary of cow name (string), weight (int) pairs
+    #Returns:
+    #a dictionary of cow name (string), weight (int) pairs
     """
 #
 #    cow_dict = dict()
@@ -89,26 +89,26 @@ def greedy_cow_transport(cows,limit=10):
     
 
 # Problem 2dict
-def brute_force_cow_transport(cows,limit=10):
-    """
-    Finds the allocation of cows that minimizes the number of spaceship trips
-    via brute force.  The brute force algorithm should follow the following method:
+#def brute_force_cow_transport(cows,limit=10):
+    #"""
+    #Finds the allocation of cows that minimizes the number of spaceship trips
+    #via brute force.  The brute force algorithm should follow the following method:
 
-    1. Enumerate all possible ways that the cows can be divided into separate trips
-    2. Select the allocation that minimizes the number of trips without making any trip
-        that does not obey the weight limitation
+    #1. Enumerate all possible ways that the cows can be divided into separate trips
+    #2. Select the allocation that minimizes the number of trips without making any trip
+        #that does not obey the weight limitation
             
-    Does not mutate the given dictionary of cows.
+    #Does not mutate the given dictionary of cows.
 
-    Parameters:
-    cows - a dictionary of name (string), weight (int) pairs
-    limit - weight limit of the spaceship (an int)
+    #Parameters:
+    #cows - a dictionary of name (string), weight (int) pairs
+    #limit - weight limit of the spaceship (an int)
     
-    Returns:
-    A list of lists, with each inner list containing the names of cows
-    transported on a particular trip and the overall list containing all the
-    trips
-    """
+    #Returns:
+    #A list of lists, with each inner list containing the names of cows
+    #transported on a particular trip and the overall list containing all the
+    #trips
+    #"""
 #    # TODO: Your code here
 #    answer = []
 #    shortest_comb = len(cows) + 1
